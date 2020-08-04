7494083fbd3751e312a9aca46dbad78bed9ad4d1
Fernando Rodriguez <fernand.roguez@gmail.com>
2020-08-04 11:18:33 -0700

    1) Model for user login 2){% csrf_token %} in the forms that implement a POST method.

--- trvgcy/pages/views.py
+++ trvgcy/pages/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    print(request.POST) ## Delete, debuging purposes!!
     return render(request, 'pages/index.html')
 
