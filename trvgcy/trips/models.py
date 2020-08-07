4ff4954991a1bc6fbe43d955508af3bb18ed857a
Fernando Rodriguez <fernand.roguez@gmail.com>
2020-08-07 05:15:11 -0700

    Configurate 'media' folder and create image fields in the database

--- trvgcy/trips/models.py
+++ trvgcy/trips/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Address(models.Model):
-
     id = models.AutoField(primary_key=True)
     continent = models.CharField(max_length=15)
     country = models.CharField(max_length=15)
@@ -13,14 +13,14 @@ class Address(models.Model):
     num = models.CharField(max_length=15)
     coordinates = models.CharField(max_length=15)
 
-class Hotel(models.Model):
 
-# Fields
+class Hotel(models.Model):
+    # Fields
 
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40, unique=True)
     chain = models.CharField(max_length=30)
-    stars = models.CharField(max_length=2)  #'5+'
+    stars = models.CharField(max_length=2)  # '5+'
     category = models.CharField(max_length=30)
     email = models.EmailField(max_length=40)
     telephone = models.CharField(max_length=15)
@@ -40,74 +40,80 @@ class Hotel(models.Model):
     has_full_board = models.BooleanField(default=False)
     has_only_breakfast = models.BooleanField(default=False)
     allow_pet = models.BooleanField(default=True)
-    rating = models.CharField(max_length=2) #'10'
-    rating_business = models.CharField(max_length=2) #'10'
-    rating_family = models.CharField(max_length=2) #'10'
-    rating_single = models.CharField(max_length=2) #'10'
-    rating_couples = models.CharField(max_length=2) #'10'
-    rating_pet = models.CharField(max_length=2) #'10'
+    rating = models.CharField(max_length=2)  # '10'
+    rating_business = models.CharField(max_length=2)  # '10'
+    rating_family = models.CharField(max_length=2)  # '10'
+    rating_single = models.CharField(max_length=2)  # '10'
+    rating_couples = models.CharField(max_length=2)  # '10'
+    rating_pet = models.CharField(max_length=2)  # '10'
+    photo_main = models.ImageField(upload_to='photos/Hotels/%Y/%m/%d/', null=True)
+    photo1 = models.ImageField(upload_to='photos/Hotels/%Y/%m/%d/', null=True)
+    photo2 = models.ImageField(upload_to='photos/Hotels/%Y/%m/%d/', null=True)
+    photo3 = models.ImageField(upload_to='photos/Hotels/%Y/%m/%d/', null=True)
+    photo4 = models.ImageField(upload_to='photos/Hotels/%Y/%m/%d/', null=True)
 
-# Metadata
+    # Metadata
 
-# Methods
+    # Methods
 
     def __str__(self):
         return self.name
 
-class Airport(models.Model):
 
+class Airport(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
 
     def __str__(self):
         return self.name
 
-class Event(models.Model):
 
+class Event(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
     category = models.CharField(max_length=30)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     is_active = models.BooleanField(default=False)
+    photo_main = models.ImageField(upload_to='photos/Events/%Y/%m/%d/', null=True)
+    photo1 = models.ImageField(upload_to='photos/Events/%Y/%m/%d/', null=True)
+    photo2 = models.ImageField(upload_to='photos/Events/%Y/%m/%d/', null=True)
+    photo3 = models.ImageField(upload_to='photos/Events/%Y/%m/%d/', null=True)
+    photo4 = models.ImageField(upload_to='photos/Events/%Y/%m/%d/', null=True)
+
 
 def __str__(self):
     return self.name
 
-class Flight(models.Model):
 
-    id = models.AutoField(primary_key = True)
+class Flight(models.Model):
+    id = models.AutoField(primary_key=True)
     airline = models.CharField(max_length=20)
     numflight = models.CharField(max_length=10)
     takeoff = models.DateTimeField()
     landing = models.DateTimeField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    is_active =models.BooleanField(default=False)
-
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.numflight
 
-class Trip(models.Model):
 
+class Trip(models.Model):
     internal_reference = models.CharField(max_length=40)
     comercial_reference = models.CharField(max_length=40)
-    #designerID = models.ForeignKey()
-    #managerID = models.ForeignKey()
+    # designerID = models.ForeignKey()
+    # managerID = models.ForeignKey()
     include_hotel = models.BooleanField(default=False)
     include_flight = models.BooleanField(default=False)
     include_event = models.BooleanField(default=False)
-    basic_price = models.DecimalField(max_digits = 5, decimal_places = 2)
+    basic_price = models.DecimalField(max_digits=5, decimal_places=2)
     stock = models.IntegerField()
     sold = models.IntegerField()
     refund = models.IntegerField()
     cancelled = models.IntegerField()
     is_active = models.BooleanField(default=False)
-
-
-
-
-
-
-
-
-
+    photo_main = models.ImageField(upload_to='photos/Trips/%Y/%m/%d/photo_main', null=True)
+    photo1 = models.ImageField(upload_to='photos/Trips/%Y/%m/%d/photo1', null=True)
+    photo2 = models.ImageField(upload_to='photos/Trips/%Y/%m/%d/photo2', null=True)
+    photo3 = models.ImageField(upload_to='photos/Trips/%Y/%m/%d/', null=True)
+    photo4 = models.ImageField(upload_to='photos/Trips/%Y/%m/%d/', null=True)
--- trvgcy/trvgcy/settings.py
+++ trvgcy/trvgcy/settings.py
@@ -124,3 +124,7 @@ USE_TZ = True
 
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
