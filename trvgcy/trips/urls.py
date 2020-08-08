d89a923b0926d9ba292c8310c44321c7566040ad
Fernando Rodriguez <fernand.roguez@gmail.com>
2020-08-08 01:11:37 -0700

    Backup trips templates

--- trvgcy/trips/urls.py
+++ trvgcy/trips/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('listing/', views.listing, name='listing'),
-    path('trip/', views.listing, name='listing')
+    path('trip/', views.trip, name='trip')
 ]
