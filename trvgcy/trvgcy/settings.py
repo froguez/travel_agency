e4d4065ab60a9a6850a5ce4f79f74b8cea949ea0
Fernando Rodriguez <fernand.roguez@gmail.com>
2020-08-10 02:01:51 -0700

    Register the new app 'checkout'

--- trvgcy/trvgcy/settings.py
+++ trvgcy/trvgcy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'checkout.apps.CheckoutConfig'
     'trips.apps.TripsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
