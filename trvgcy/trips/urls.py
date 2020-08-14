1ff814fa4fb8e9ec2672501e823e8972023bc7e1
Fernando Rodriguez <fernand.roguez@gmail.com>
2020-08-14 15:54:39 -0700

    Redirecting practise

--- trvgcy/trips/urls.py
+++ trvgcy/trips/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'trips'
+
 urlpatterns = [
     path('search/', views.search, name='search'),
     path('listing/', views.listing, name='listing'),
-    path('trip/<int:trip_id>', views.trip, name='trip')
+    path('trip/<int:trip_id>/', views.trip, name='trip')
 ]
