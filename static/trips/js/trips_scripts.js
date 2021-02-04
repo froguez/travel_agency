a3639ad173e5b07dc80693ade3fb54fe85cc4ab6
Fernando Rodriguez <fernand.roguez@gmail.com>
2021-02-04 21:40:50 +0200

    full uploading of static files to git

---
+++ static/trips/js/trips_scripts.js
@@ -0,0 +1,1 @@
+console.log('test trips_scripts.js')
---
+++ trvgcy/trips/static/trips/js/trips_scripts.js
@@ -0,0 +1,13 @@
+
+$(document).ready( function () {
+
+    // Datatable _tabsOnBuilder
+
+    $('#table1').DataTable();
+    $('#table2').DataTable();
+    $('#table3').DataTable();
+    $('#table4').DataTable();
+    $('#table5').DataTable();
+    $('#table6').DataTable();
+});
+
