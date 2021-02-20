b57e1b83666edbf56821d3523c418aaa3a4233d1
Fernando Rodriguez <fernand.roguez@gmail.com>
2021-02-20 05:28:22 +0200

    Install Django REST Framework

--- trvgcy/trvgcy/settings.py
+++ trvgcy/trvgcy/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize'
+    'django.contrib.humanize',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
