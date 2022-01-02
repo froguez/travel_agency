1fac118c8a06a46ec2180d3a82d1b6e0b9bc4a2c
Fernando Rodriguez <fernand.roguez@gmail.com>
2022-01-02 17:12:57 +0200

    Added 'local_settings' Import

--- trvgcy/trvgcy/settings.py
+++ trvgcy/trvgcy/settings.py
@@ -131,3 +131,10 @@ STATIC_URL = '/static/'
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Adding local settings import
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
